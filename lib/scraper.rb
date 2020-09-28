d2d2f9e7e607f38a267aef964c445009131c434c
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-28 14:23:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,10 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    @students = []
+    doc = Nokogiri::HTML(open(index_url))
+    binding.pry
+    name = doc.css(".student-card" ".student-name").text.strip
   end
 
   def self.scrape_profile_page(profile_url)
